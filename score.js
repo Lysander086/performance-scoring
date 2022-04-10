4f9f54ad211634ae1379ea7ad459d7de8cf58a21
Lysander <19286863+Lysander686@users.noreply.github.com>
2022-04-10 17:58:36 +0800

    remove easy mode

--- score.js
+++ score.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let fileName = "test node write.csv";
+let fileName = "report.csv";
 
 fs.rmSync(fileName, {
     force: true,
@@ -7,7 +7,6 @@ fs.rmSync(fileName, {
 
 
 let learningHours = 22.25,
-    wantedHour = 25,
     hardWantedHours = 28,
     noMas = 5,
     exercise = 6,
@@ -16,30 +15,24 @@ let learningHours = 22.25,
     nWeight = 0.04,
     eWeight = 0.16;
 
-let finalScore =
-    (learningHours / wantedHour) * lWeight +
+
+let ultimateScore = (learningHours / hardWantedHours) * lWeight +
     (noMas / days) * nWeight +
     (exercise / days) * eWeight;
 
-let hardFinal = (learningHours / hardWantedHours) * lWeight +
-    (noMas / days) * nWeight +
-    (exercise / days) * eWeight;
-
-finalScore = parseFloat(finalScore).toFixed(2);
-hardFinal = parseFloat(hardFinal).toFixed(2);
+ultimateScore = parseFloat(ultimateScore).toFixed(2);
 
 
-let learningHourScore = parseFloat(`${learningHours / wantedHour}`).toFixed(2);
 let noMasCore = parseFloat(`${noMas / days}`).toFixed(2);
 let exerciseScore = parseFloat(`${exercise / days}`).toFixed(2);
 
 
 let doc =
     `,percent ,score ,weight
-learning hour: ,${learningHours + " | " + wantedHour + "|" + hardWantedHours},${learningHourScore}, ${lWeight}
+learning hour: ,${learningHours + " | " +   hardWantedHours}, , ${lWeight}
 noMas: ,${noMas + " | " + days},${noMasCore}, ${nWeight}
 exercise: ,${exercise + " | " + days},${exerciseScore}, ${eWeight}
-final: , ,${hardFinal} (h) ,${finalScore} (e)`;
+final: , , , ${ultimateScore} (h)`;
 
 fs.writeFile(fileName, doc, () => {
     console.log("Report generated.");
