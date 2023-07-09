080920f1858a38e8865ca7c749230b395e1c63ef
Lysander <19286863+Lysander686@users.noreply.github.com>
2023-07-09 22:21:46 +0900

    nm

---
+++ month report .js
@@ -0,0 +1,7 @@
+
+let focusHours = 14.25 + 12 + 11.75 + 21.5,
+    hardWantedHours = 33 + 35 + 39 + 39;
+
+    console.log(`focusHours | hardWantedHours: ${focusHours}|${hardWantedHours}` );
+console.log(focusHours / hardWantedHours);
+
--- score.js
+++ score.js
@@ -1,3 +1,4 @@
+// This code is creating a report in a CSV file. It is calculating a score based on the number of hours spent focusing, the number of days of noMas, and the number of days of exercise. The score is calculated by taking the ratio of focus hours to hard wanted hours, multiplied by a weight, and adding the ratio of noMas days to total days, multiplied by a weight, and adding the ratio of exercise days to total days, multiplied by a weight. The final score is then written to the CSV file.
 const fs = require("fs");
 let fileName = "report.csv";
 
@@ -5,32 +6,35 @@ fs.rmSync(fileName, {
     force: true,
 });
 
-
-let focusHours = 24,
-    hardWantedHours = 3,
-    noMas = 5,
-    exercise = 4,
-    days = 7,
+// try to interpret the below code
+let focusHours = 13.75,
+    hardWantedHours =  30,
+    noMas = 4,
+    exercise = 9,
     lWeight = 0.8,
     nWeight = 0.04,
     eWeight = 0.16;
 
+let days = 7;
+
+const exerciseTimes = 4 + 10;
+
 let ultimateScore = (focusHours / hardWantedHours) * lWeight +
     (noMas / days) * nWeight +
-    (exercise / days) * eWeight;
+    (exercise / exerciseTimes) * eWeight;
 
 ultimateScore = parseFloat(ultimateScore).toFixed(2);
 
 let learningScore = parseFloat(`${focusHours / hardWantedHours}`).toFixed(2);
 let noMasCore = parseFloat(`${noMas / days}`).toFixed(2);
-let exerciseScore = parseFloat(`${exercise / days}`).toFixed(2);
+let exerciseScore = parseFloat(`${exercise / exerciseTimes}`).toFixed(2);
 
 
 let doc =
     `,percent ,score ,weight
 learning hour: ,${focusHours + " | " + hardWantedHours}, ${learningScore}, ${lWeight}
 noMas: ,${noMas + " | " + days},${noMasCore}, ${nWeight}
-exercise: ,${exercise + " | " + days},${exerciseScore}, ${eWeight}
+exercise: ,${exercise + " | " + exerciseTimes},${exerciseScore}, ${eWeight}
 final: , , , ${ultimateScore} (h)`;
 
 fs.writeFile(fileName, doc, () => {
